658a9e56496df14b3664b0091f805b855051ecbe
Stefysp <stefaniapantina@gmail.com>
2018-10-25 10:52:23 +0200

    test js

---
+++ test.js
@@ -0,0 +1,8 @@
+function show1(){
+  document.getElementById('shcompany').style.display ='none';
+  document.getElementById("other").value = ""; 
+}
+function show2(){
+  document.getElementById('shcompany').style.display = 'block';
+}
+            
